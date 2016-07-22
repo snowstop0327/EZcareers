2de7f04698b9ce407d5aacb2cfe7061447a10ee8
snowstop19900327 <hst-790327@hotmail.com>
2016-07-22 11:09:11 +0800

    plus-hide-js

--- javascript/EZcareers.js
+++ javascript/EZcareers.js
@@ -3,88 +3,98 @@
  */
 
 $(document).ready(function() {
-  $('.jobs-list > li').click(function(event) {
-    // event.preventDefault();
-    // $(this).siblings().find('p').slideUp();
-    $(this).find('p').slideToggle('fast');
-    $(this).toggleClass('gray-bg');
-    	toggleNavigation();
-  });
-});
-function toggleNavigation() {
-    if ($('.jobs-list > li').hasClass('icon-android-add')) {
-        // Close Nav
-        $(this).removeClass('icon-android-add');
-    } else {
-        // Open Nav
-        $(this).addClass('icon-android-remove');
-    }
-}    
-
-/*
- * Sticky header
- */
-
-$(function () {
-
-    $('.EZcareers-header').each(function () {
-
-        var $window = $(window), // 將window轉為jQuery物件
-            $header = $(this),   // 將header轉為jQuery物件
-        // 取得header的預設位置
-            headerOffsetTop = $header.offset().top;
-
-        // 監控視窗捲動事件
-        // (每次視窗觸發捲動事件時執行處理)
-        $window.on('scroll', function () {
-            // 檢查視窗捲動程度、
-            // 若超過header的預設位置則附加類別、
-            // 反之則刪除
-            if ($window.scrollTop() > headerOffsetTop) {
-                $header.addClass('sticky');
-            } else {
-                $header.removeClass('sticky');
-            }
-        });
-        // debugger;
-        // 觸發window的捲動事件
-        // (用以調整header的初始位置)
-        //$window.trigger('scroll');
-
+    $('.jobs-list > li').click(function(event) {
+        // event.preventDefault();
+        // $(this).siblings().find('p').slideUp();
+        $(this).find('p').slideToggle('fast');
+        $(this).toggleClass('gray-bg');
+        var $currIcon = $(this).find("i.the-btn");
+        $currIcon.toggleClass('icon-android-remove icon-android-add');
+        
     });
 });
 
-/*
- * Sticky fire
- */
+// SLiding codes
+        // $(".jobs-list > li").click(function() {
+        //     if (false == $(this).next().is(':visible')) {
+        //         $(this).find('p').slideUp();
+        //     }
 
-$(function () {
+        //     var $currIcon = $(this).find("i.the-btn");
 
-    $('#nn-title').each(function () {
+        //     $("i.the-btn").not($currIcon).addClass('icon-android-remove').removeClass('icon-android-add');
 
-        var $window = $(window), // 將window轉為jQuery物件
-            $fire = $(this),   // 將header轉為jQuery物件
-        // 取得header的預設位置
-            headerOffsetTop = $fire.offset().top;
+        //     $currIcon.toggleClass('icon-android-add icon-android-remove');
 
-        // 監控視窗捲動事件
-        // (每次視窗觸發捲動事件時執行處理)
-        $window.on('scroll', function () {
-            // 檢查視窗捲動程度、
-            // 若超過header的預設位置則附加類別、
-            // 反之則刪除
-            if ($window.scrollTop() > headerOffsetTop) {
-                $fire.hide();
-                $('svg').show();
-            } else {
-                $fire.show();
-                $('svg').hide();
-            }
+        //     $(this).next().slideToggle();
+
+        //     $(this).toggleClass('gray-bg');
+
+        // });
+        /*
+         * Sticky header
+         */
+
+        $(function() {
+
+            $('.EZcareers-header').each(function() {
+
+                var $window = $(window), // 將window轉為jQuery物件
+                    $header = $(this), // 將header轉為jQuery物件
+                    // 取得header的預設位置
+                    headerOffsetTop = $header.offset().top;
+
+                // 監控視窗捲動事件
+                // (每次視窗觸發捲動事件時執行處理)
+                $window.on('scroll', function() {
+                    // 檢查視窗捲動程度、
+                    // 若超過header的預設位置則附加類別、
+                    // 反之則刪除
+                    if ($window.scrollTop() > headerOffsetTop) {
+                        $header.addClass('sticky');
+                    } else {
+                        $header.removeClass('sticky');
+                    }
+                });
+                // debugger;
+                // 觸發window的捲動事件
+                // (用以調整header的初始位置)
+                //$window.trigger('scroll');
+
+            });
         });
-        // debugger;
-        // 觸發window的捲動事件
-        // (用以調整header的初始位置)
-        //$window.trigger('scroll');
 
-    });
-});
+        /*
+         * Sticky fire
+         */
+
+        $(function() {
+
+            $('#nn-title').each(function() {
+
+                var $window = $(window), // 將window轉為jQuery物件
+                    $fire = $(this), // 將header轉為jQuery物件
+                    // 取得header的預設位置
+                    headerOffsetTop = $fire.offset().top;
+
+                // 監控視窗捲動事件
+                // (每次視窗觸發捲動事件時執行處理)
+                $window.on('scroll', function() {
+                    // 檢查視窗捲動程度、
+                    // 若超過header的預設位置則附加類別、
+                    // 反之則刪除
+                    if ($window.scrollTop() > headerOffsetTop) {
+                        $fire.hide();
+                        $('svg').show();
+                    } else {
+                        $fire.show();
+                        $('svg').hide();
+                    }
+                });
+                // debugger;
+                // 觸發window的捲動事件
+                // (用以調整header的初始位置)
+                //$window.trigger('scroll');
+
+            });
+        });
